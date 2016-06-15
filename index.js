f1d1d4241d7b977bcaa814918671489cc7c965c7
Tommy <meier@rhinos-media.de>
2016-06-15 09:57:19 +0200

    basic logic to parse and validate arguments and configuration file

--- index.js
+++ index.js
@@ -1,5 +1,67 @@
+/**
+ * node index.js <module> [action=all] [--help]
+ */
 ;(function() {
     "use strict";
 
-    console.log('hello world');
+    // parse arguments
+    var args   = process.argv.slice(2),
+        module = '',
+        action = '';
+
+    args.forEach(function(value) {
+        if (value == '--help')
+        {
+            console.log('Syntax: buildweb <module> [action=all] [--help]');
+            process.exit();
+        }
+        else if(!module.length)
+        {
+            module = value;
+        }
+        else if(!action.length)
+        {
+            action = value;
+        }
+    });
+
+    action = action || 'all';
+    if (module.length == 0)
+    {
+        console.log('Missing argument: module');
+        return;
+    }
+
+    // load configuration file
+    var fs = require('fs');
+    fs.exists('buildweb.json', function(exists) {
+        if (!exists)
+        {
+             console.log('buildweb.json not found');
+             process.exit();
+        }
+
+        fs.readFile('buildweb.json', 'utf8', function(error, contents) {
+            var config = JSON.parse(contents);
+            if (config[module] === undefined)
+            {
+                console.log('Module %s not found in config file.', module);
+                process.exit();
+            }
+
+            if (action == 'all')
+            {
+                console.log('executing all actions for module %s', module);
+            }
+            else if(config[module][action] !== undefined)
+            {
+                console.log('executing %s.%s', module, action);
+            }
+            else
+            {
+                console.log('Action %s in module %s not found.', action, module);
+                process.exit();
+            }
+        });
+    });
 })();
